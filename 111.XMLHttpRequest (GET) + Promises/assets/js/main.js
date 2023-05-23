a2bd13e3ee0b35d594ec16b25999ea573bbac00e
Gabriel Mello <gabriel.mellosjb@gmail.com>
2023-05-23 16:53:21 -0300

    ErrorTest apagado

--- 111.XMLHttpRequest (GET) + Promises/assets/js/main.js
+++ 111.XMLHttpRequest (GET) + Promises/assets/js/main.js
@@ -6,7 +6,7 @@ const request = obj => {
         xhr.send(null);
     
         xhr.addEventListener('load', () => {
-            if (xhr.status >= 200 && xhr.status < 300 && errorTest) {
+            if (xhr.status >= 200 && xhr.status < 300) {
                 resolve(xhr.responseText);
             } else {
                 reject({
