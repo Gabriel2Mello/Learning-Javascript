c5344755a1f5df1ed791aa79f154364150f031bf
Gabriel Mello <gabriel.mellosjb@gmail.com>
2023-05-24 15:35:18 -0300

    Webpack (Boilerplate)

---
+++ 116.Webpack (Boilerplate)/public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// npm run loadWebpack
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
