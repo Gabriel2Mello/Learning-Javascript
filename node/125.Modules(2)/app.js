f89e2e2fb234e48f6a567ca40dd2ea3587f84c2f
Gabriel Mello <gabriel.mellosjb@gmail.com>
2023-05-26 14:16:59 -0300

    paths with node

---
+++ node/125.Modules(2)/app.js
@@ -0,0 +1,8 @@
+const { multiplicacao } = require('./Z/mod2');
+
+const path = require('path');
+
+// console.log(__filename);
+// console.log(__dirname);
+
+console.log(path.resolve(__dirname, '..', '..', 'modelo'));
