5fd35b872369d5f85e7df8228a77abbd0f6ed8c5
Gabriel Mello <gabriel.mellosjb@gmail.com>
2023-05-26 16:42:39 -0300

    Express(introduction)

---
+++ node/130.Express(Introduction)/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send(`
+    <form action="/" method="POST">
+    Nome: <input type="text" name="nome">
+    <button>Enviar</button>
+    </form>
+    `);
+});
+
+app.post('/', (request, response) => {
+    response.send('Form sent');
+});
+
+app.get('/contact', (request, response) => {
+    response.send('Thank you for the contact');
+});
+
+app.listen(3000, () => {
+    console.log('Server executing...');
+    console.log('Access: http://localhost:3000');
+});
